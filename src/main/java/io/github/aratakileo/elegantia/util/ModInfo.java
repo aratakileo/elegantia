4b77fe141e467b2d2a25080ba344411c2a4a5dc3
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2024-02-22 22:30:09 +0300

    Update ModInfo.java

--- src/main/java/io/github/aratakileo/elegantia/util/ModInfo.java
+++ src/main/java/io/github/aratakileo/elegantia/util/ModInfo.java
@@ -5,20 +5,67 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import io.github.aratakileo.elegantia.Elegantia;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.metadata.Person;
 import net.minecraft.client.gui.screens.Screen;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
-public interface ModInfo {
-    @NotNull String getVersion();
-    @NotNull String getName();
-    @NotNull Optional<String> getSourcesUrl();
+public abstract class ModInfo {
+    public abstract @NotNull String getId();
+    public abstract @NotNull String getVersion();
+    public abstract @NotNull String getName();
+    public abstract @NotNull String getDescription();
+    public abstract @NotNull Collection<String> getAuthors();
+    public abstract @NotNull Collection<String> getContributors();
+    public abstract @NotNull Optional<Map<String, String>> getUrls();
+    public abstract @NotNull Environment getEnvironment();
 
-    static @NotNull Optional<ModInfo> get(@NotNull String modId) {
+    public @NotNull Optional<String> getUrl(@NotNull String key) {
+        return getUrls().map(map -> map.get(key));
+    }
+
+    public @NotNull Optional<String> getSourcesUrl() {
+        return getUrl("sources");
+    }
+
+    public @NotNull Optional<String> getIssuesUrl() {
+        return getUrl("issues");
+    }
+
+    public @NotNull Optional<String> getHomepageUrl() {
+        return getUrl("homepage");
+    }
+
+    @SuppressWarnings("unchecked")
+    public void setConfigScreenGetter(@NotNull Function<Screen, Screen> configScreenGetter) {
+        if (!isModLoaded("modmenu")) return;
+
+        try {
+            /*
+             * Adding configuration screens is necessary in this way, due to the fact
+             * that the `ModMenuApi#getProvidedConfigScreenFactories` interface works every other time
+             */
+            final var field = ModMenu.class.getDeclaredField("configScreenFactories");
+            field.setAccessible(true);
+
+            final var configScreenFactories = (Map<String, ConfigScreenFactory<?>>) field.get(null);
+            configScreenFactories.put(getId(), configScreenGetter::apply);
+
+            field.set(null, configScreenFactories);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Elegantia.LOGGER.error(
+                    "Something went wrong while trying to set config screen getter for mod id `" + getId() + '`',
+                    e
+            );
+        }
+    }
+
+    public static @NotNull Optional<ModInfo> get(@NotNull String modId) {
         final var modMetadata = FabricLoader.getInstance()
                 .getModContainer(modId)
                 .map(ModContainer::getMetadata)
@@ -28,6 +75,11 @@ public interface ModInfo {
             return Optional.empty();
 
         return Optional.of(new ModInfo() {
+            @Override
+            public @NotNull String getId() {
+                return modMetadata.getId();
+            }
+
             @Override
             public @NotNull String getVersion() {
                 return modMetadata.getVersion().getFriendlyString();
@@ -39,26 +91,84 @@ public interface ModInfo {
             }
 
             @Override
-            public @NotNull Optional<String> getSourcesUrl() {
-                return modMetadata.getContact().get("sources");
+            public @NotNull String getDescription() {
+                return modMetadata.getDescription();
+            }
+
+            @Override
+            public @NotNull Collection<String> getAuthors() {
+                return modMetadata.getAuthors().stream().map(Person::getName).toList();
+            }
+
+            @Override
+            public @NotNull Collection<String> getContributors() {
+                return modMetadata.getContributors().stream().map(Person::getName).toList();
+            }
+
+            @Override
+            public @NotNull Optional<Map<String, String>> getUrls() {
+                return Optional.ofNullable(modMetadata.getContact().asMap());
+            }
+
+            @Override
+            public @NotNull Environment getEnvironment() {
+                return switch (modMetadata.getEnvironment()) {
+                    case CLIENT -> Environment.CLIENT;
+                    case SERVER -> Environment.SERVER;
+                    case UNIVERSAL -> Environment.BOTH;
+                };
             }
         });
     }
 
-    static @NotNull Optional<String> getName(@NotNull String modId) {
-        return get(modId).map(ModInfo::getName);
-    }
-
-    static @NotNull Optional<String> getVersion(@NotNull String modId) {
+    public static @NotNull Optional<String> getVersion(@NotNull String modId) {
         return get(modId).map(ModInfo::getVersion);
     }
 
-    static @NotNull Optional<String> getSourcesUrl(@NotNull String modId) {
+    public static @NotNull Optional<String> getName(@NotNull String modId) {
+        return get(modId).map(ModInfo::getName);
+    }
+
+    public static @NotNull Optional<String> getDescription(@NotNull String modId) {
+        return get(modId).map(ModInfo::getDescription);
+    }
+
+    public static @NotNull Optional<Collection<String>> getAuthors(@NotNull String modId) {
+        return get(modId).map(ModInfo::getAuthors);
+    }
+
+    public static @NotNull Optional<Collection<String>> getContributors(@NotNull String modId) {
+        return get(modId).map(ModInfo::getContributors);
+    }
+
+    public static @NotNull Optional<Map<String, String>> getUrls(@NotNull String modId) {
+        return get(modId).flatMap(ModInfo::getUrls);
+    }
+
+    public static @NotNull Optional<String> getUrl(@NotNull String modId, @NotNull String key) {
+        return get(modId).flatMap(modInfo -> modInfo.getUrl(key));
+    }
+
+    public static @NotNull Optional<String> getSourcesUrl(@NotNull String modId) {
         return get(modId).flatMap(ModInfo::getSourcesUrl);
     }
 
-    @SuppressWarnings("unchecked")
-    static void setConfigScreenGetter(@NotNull String modId, @NotNull Function<Screen, Screen> configScreenGetter) {
+    public static @NotNull Optional<String> getIssuesUrl(@NotNull String modId) {
+        return get(modId).flatMap(ModInfo::getIssuesUrl);
+    }
+
+    public static @NotNull Optional<String> getHomepageUrl(@NotNull String modId) {
+        return get(modId).flatMap(ModInfo::getHomepageUrl);
+    }
+
+    public static @NotNull Optional<Environment> getEnvironment(@NotNull String modId) {
+        return get(modId).map(ModInfo::getEnvironment);
+    }
+
+    public static void setConfigScreenGetter(
+            @NotNull String modId,
+            @NotNull Function<Screen, Screen> configScreenGetter
+    ) {
         if (!isModLoaded("modmenu")) return;
 
         if (!isModLoaded(modId)) {
@@ -70,27 +180,16 @@ public interface ModInfo {
             return;
         }
 
-        try {
-            /*
-             * Adding configuration screens is necessary in this way, due to the fact
-             * that the `ModMenuApi#getProvidedConfigScreenFactories` interface works every other time
-             */
-            final var field = ModMenu.class.getDeclaredField("configScreenFactories");
-            field.setAccessible(true);
-
-            final var configScreenFactories = (Map<String, ConfigScreenFactory<?>>) field.get(null);
-            configScreenFactories.put(modId, configScreenGetter::apply);
-
-            field.set(null, configScreenFactories);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            Elegantia.LOGGER.error(
-                    "Something went wrong while trying to set config screen getter for mod id `" + modId + '`',
-                    e
-            );
-        }
+        get(modId).orElseThrow().setConfigScreenGetter(configScreenGetter);
     }
 
-    static boolean isModLoaded(@NotNull String modId) {
+    public static boolean isModLoaded(@NotNull String modId) {
         return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    public enum Environment {
+        CLIENT,
+        SERVER,
+        BOTH;
     }
 }
