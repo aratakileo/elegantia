70f0f580fbf1e840c38d01255d415877f990e1b3
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2024-08-04 20:37:44 +0300

    Create ResultSlot.java

---
+++ src/main/java/io/github/aratakileo/elegantia/gui/slot/ResultSlot.java
@@ -0,0 +1,17 @@
+package io.github.aratakileo.elegantia.gui.slot;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class ResultSlot extends Slot {
+    public ResultSlot(@NotNull Container container, int index, int xPos, int yPos) {
+        super(container, index, xPos, yPos);
+    }
+
+    @Override
+    public boolean mayPlace(@NotNull ItemStack itemStack) {
+        return false;
+    }
+}
